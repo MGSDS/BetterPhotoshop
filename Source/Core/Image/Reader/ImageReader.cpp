25ede2f76d6951925c2f154864cdcdddb024cd54
Winter091 <winter091@yandex.ru>
2022-09-28 19:09:04 +0300

    refactor: use map for mapping format to reader

--- Source/Core/Image/Reader/ImageReader.cpp
+++ Source/Core/Image/Reader/ImageReader.cpp
@@ -4,16 +4,28 @@
 #include <Core/Utils/Utils.hpp>
 #include <Core/Image/Reader/PgmReader.hpp>
 #include <Core/Image/Reader/PpmReader.hpp>
+#include <Core/Image/ImageFormat.hpp>
+
+#include <unordered_map>
+
+static std::unordered_map<std::string, ImageFormat> EXTENSION_TO_IMAGE_FORMAT = {
+    { ".pgm", ImageFormat::Pgm },
+    { ".ppm", ImageFormat::Ppm },
+};
 
 std::unique_ptr<ImageReader> ImageReader::GetReader(const std::string& fileExtension)
 {
     std::string extension = Utils::ToLower(fileExtension);
-    
-    if (extension == ".pgm") {
-        return std::make_unique<PgmReader>();
+ 
+    auto format = EXTENSION_TO_IMAGE_FORMAT.find(extension);
+    if (format == EXTENSION_TO_IMAGE_FORMAT.end()) {
+        return nullptr;
     }
-    if (extension == ".ppm") {
-        return std::make_unique<PpmReader>();
+
+    switch(format->second) {
+        case ImageFormat::Pgm:  return std::make_unique<PgmReader>();
+        case ImageFormat::Ppm:  return std::make_unique<PpmReader>();
+        default: break;
     }
 
     return nullptr;
