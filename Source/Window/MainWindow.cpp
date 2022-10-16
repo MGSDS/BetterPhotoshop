45c97f79656da1fde6893a81263931911b109325
Dmitry Voevodskiy <33420667+Winter091@users.noreply.github.com>
2022-10-16 16:58:38 +0300

    Feat: Convert image models using GUI (#38)
    * feat: add gui menus to convert image models
    
    * feat: convert color spaces when saving/opening/creating images

--- Source/Window/MainWindow.cpp
+++ Source/Window/MainWindow.cpp
@@ -1,7 +1,7 @@
 #include "MainWindow.hpp"
 
+#include <Core/Image/ColorModel/ColorModelConverter.hpp>
 #include <Core/Image/Image.hpp>
-
 #include <Core/Log.hpp>
 
 #include <QDialog>
@@ -15,8 +15,11 @@ QSize NEW_IMAGE_MIN_SIZE = { 1, 1 };
 QSize NEW_IMAGE_MAX_SIZE = { 32768, 32768 };
 QSize NEW_IMAGE_DEFAULT_SIZE = { 1280, 720 };
 std::string NEW_IMAGE_DEFAULT_NAME = "Untitled";
+ColorModel DEFAULT_COLOR_MODEL = ColorModel::RGB;
 
 MainWindow::MainWindow(const WindowSettings& settings)
+    : m_BaseTitle(settings.Title)
+    , m_SelectedColorModel(DEFAULT_COLOR_MODEL)
 {
     resize(settings.Width, settings.Height);
 
@@ -24,7 +27,6 @@ MainWindow::MainWindow(const WindowSettings& settings)
         setWindowState(Qt::WindowMaximized);
     }
 
-    m_BaseTitle = settings.Title;
     setWindowTitle(m_BaseTitle.c_str());
 
     InitMenuBar();
@@ -37,24 +39,51 @@ void MainWindow::InitMenuBar()
     auto* menu = menuBar();
 
     auto* fileMenu = menu->addMenu("File");
+    {
+        auto* newAction = fileMenu->addAction("New");
+        newAction->setShortcut(QKeySequence::New);
+        connect(newAction, &QAction::triggered, this, &MainWindow::OnFileNewAction);
 
-    auto* newAction = fileMenu->addAction("New");
-    newAction->setShortcut(QKeySequence::New);
-    connect(newAction, &QAction::triggered, this, &MainWindow::OnFileNewAction);
+        auto* openAction = fileMenu->addAction("Open");
+        openAction->setShortcut(QKeySequence::Open);
+        connect(openAction, &QAction::triggered, this, &MainWindow::OnFileOpenAction);
 
-    auto* openAction = fileMenu->addAction("Open");
-    openAction->setShortcut(QKeySequence::Open);
-    connect(openAction, &QAction::triggered, this, &MainWindow::OnFileOpenAction);
+        m_SaveAction = fileMenu->addAction("Save");
+        m_SaveAction->setShortcut(QKeySequence::Save);
+        m_SaveAction->setEnabled(false);
+        connect(m_SaveAction, &QAction::triggered, this, &MainWindow::OnFileSaveAction);
 
-    m_SaveAction = fileMenu->addAction("Save");
-    m_SaveAction->setShortcut(QKeySequence::Save);
-    m_SaveAction->setEnabled(false);
-    connect(m_SaveAction, &QAction::triggered, this, &MainWindow::OnFileSaveAction);
+        m_SaveAsAction = fileMenu->addAction("Save As");
+        m_SaveAsAction->setShortcut(QKeySequence::SaveAs);
+        m_SaveAsAction->setEnabled(false);
+        connect(m_SaveAsAction, &QAction::triggered, this, &MainWindow::OnFileSaveAsAction);
+    }
 
-    m_SaveAsAction = fileMenu->addAction("Save As");
-    m_SaveAsAction->setShortcut(QKeySequence::SaveAs);
-    m_SaveAsAction->setEnabled(false);
-    connect(m_SaveAsAction, &QAction::triggered, this, &MainWindow::OnFileSaveAsAction);
+    auto* imageMenu = menu->addMenu("Image");
+    {
+        auto* colorModelMenu = imageMenu->addMenu("Color Model");
+        {
+            auto* colorModelActionGroup = new QActionGroup(this);
+            colorModelActionGroup->setExclusive(true);
+
+            for (const auto& [colorModelStr, colorModelEnum] : STRING_TO_ENUM_COLOR_MODEL_MAPPING) {
+                auto* modelAction = colorModelMenu->addAction(colorModelStr.c_str());
+                modelAction->setCheckable(true);
+                colorModelActionGroup->addAction(modelAction);
+
+                if (colorModelEnum == DEFAULT_COLOR_MODEL) {
+                    modelAction->setChecked(true);
+                    m_DefaultColorModelAction = modelAction;
+                }
+
+                connect(modelAction, &QAction::toggled, this, [this, model = colorModelEnum](bool checked) {
+                    if (checked) {
+                        OnImageColorModelActionSelected(model);
+                    }
+                });
+            }
+        }
+    }
 }
 
 void MainWindow::InitImageView()
@@ -126,7 +155,10 @@ void MainWindow::OnFileNewAction()
 
     int width = spinBoxWidth->value();
     int height = spinBoxHeight->value();
-    Log::Debug("New image width: {}, height: {}", width, height);
+    Log::Debug("OnFileNewAction: New image width: {}, height: {}", width, height);
+
+    m_SelectedColorModel = DEFAULT_COLOR_MODEL;
+    m_DefaultColorModelAction->setChecked(true);
 
     SetImage(std::make_unique<Image>(width, height));
     SetImagePath(std::string());
@@ -146,17 +178,25 @@ void MainWindow::OnFileOpenAction()
         return;
     }
 
-    Log::Debug("Trying to read image from file: {}", filename.toStdString().c_str());
+    Log::Debug("OnFileOpenAction: Trying to read image from file: {}", filename.toStdString().c_str());
 
     LoadedImageData imageData;
     try {
         imageData = Image::FromFile(filename.toStdString());
-        SetImage(std::move(imageData.LoadedImage));
     } catch (const std::exception& exception) {
-        Log::Error("An error occured while reading image from file: {}", exception.what());
+        Log::Error("OnFileOpenAction: An error occured while reading image from file: {}", exception.what());
         return;
     }
 
+    Log::Debug("OnFileOpenAction: successufully loaded image");
+    std::unique_ptr<Image> loadedImage = std::move(imageData.LoadedImage);
+
+    if (m_SelectedColorModel != ColorModel::RGB && imageData.Format != ImageFormat::Ppm) {
+        Log::Debug("OnFileOpenAction: need to convert image to current color space");
+        loadedImage = ConvertImageToNewModel(*loadedImage, ColorModel::RGB, m_SelectedColorModel);
+    }
+
+    SetImage(std::move(loadedImage));
     SetImagePath(filename.toStdString());
     m_LastSelectedSaveFormat = imageData.Format;
 }
@@ -171,7 +211,7 @@ void MainWindow::OnFileSaveAction()
     if (m_ImagePath.empty()) {
         OnFileSaveAsAction();
     } else {
-        TrySaveCurrentImage(m_ImagePath, m_LastSelectedSaveFormat);
+        TrySaveImage(*m_Image, m_ImagePath, m_LastSelectedSaveFormat);
     }
 }
 
@@ -201,9 +241,23 @@ void MainWindow::OnFileSaveAsAction()
         filename.append(selectedFileExtension);
     }
 
-    TrySaveCurrentImage(filename.toStdString(), selectedImageFormat);
+    TrySaveImage(*m_Image, filename.toStdString(), selectedImageFormat);
     SetImagePath(filename.toStdString());
     m_LastSelectedSaveFormat = selectedImageFormat;
+}
+
+void MainWindow::OnImageColorModelActionSelected(ColorModel selectedColorModel)
+{
+    if (selectedColorModel == m_SelectedColorModel) {
+        return;
+    }
+
+    ColorModel prevModel = m_SelectedColorModel;
+    m_SelectedColorModel = selectedColorModel;
+
+    if (m_Image) {
+        SetImage(ConvertImageToNewModel(*m_Image, prevModel, m_SelectedColorModel));
+    }
 }
 
 void MainWindow::SetImage(std::unique_ptr<Image>&& image)
@@ -216,16 +270,28 @@ void MainWindow::SetImage(std::unique_ptr<Image>&& image)
     m_SaveAsAction->setEnabled(enableSaveActions);
 }
 
-bool MainWindow::TrySaveCurrentImage(const std::string& filename, ImageFormat format)
+bool MainWindow::TrySaveImage(const Image& image, const std::string& filename, ImageFormat format)
 {
-    Log::Debug("TrySaveCurrentImage: writing image to file: {}", filename);
+    if (m_SelectedColorModel == ColorModel::RGB || format == ImageFormat::Ppm) {
+        Log::Debug("TrySaveImage: no need to convert image to rgb color model");
+        return TrySaveRgbImage(image, filename, format);
+    }
+
+    Log::Debug("TrySaveImage: converting image to rgb color model");
+    auto rgbImage = ConvertImageToNewModel(image, m_SelectedColorModel, ColorModel::RGB);
+    return TrySaveRgbImage(*rgbImage, filename, format);
+}
+
+bool MainWindow::TrySaveRgbImage(const Image& image, const std::string& filename, ImageFormat format)
+{
+    Log::Debug("TrySaveRgbImage: writing image to file: {}", filename);
 
     try {
-        m_Image->WriteToFile(filename, format);
-        Log::Debug("TrySaveCurrentImage: image is successfully saved");
+        image.WriteToFile(filename, format);
+        Log::Debug("TrySaveRgbImage: image is successfully saved");
         return true;
     } catch (const std::exception& exception) {
-        Log::Error("TrySaveCurrentImage: an error occured while saving image to file: {}", exception.what());
+        Log::Error("TrySaveRgbImage: an error occured while saving image to file: {}", exception.what());
         return false;
     }
 }
@@ -238,8 +304,18 @@ void MainWindow::SetImagePath(const std::string& newPath)
     setWindowTitle((m_BaseTitle + " - " + imageName).c_str());
 }
 
-void MainWindow::resizeEvent(QResizeEvent* event)
+std::unique_ptr<Image> MainWindow::ConvertImageToNewModel(const Image& image, ColorModel currentModel, ColorModel newModel)
 {
-    const QSize& newSize = event->size();
-    Log::Debug("Window resize: {}, {}", newSize.width(), newSize.height());
+    Log::Debug("ConvertImageToNewModel: converting {} to {}", static_cast<uint32_t>(currentModel), static_cast<uint32_t>(newModel));
+
+    if (currentModel == newModel) {
+        return std::make_unique<Image>(image);
+    }
+
+    auto toRGBConverter = ColorModelConverter::GetConverter(currentModel);
+    auto rgbImage = toRGBConverter->ToRGB(image);
+    auto fromRGBConverter = ColorModelConverter::GetConverter(newModel);
+    auto newImage = fromRGBConverter->FromRGB(*rgbImage);
+
+    return newImage;
 }
