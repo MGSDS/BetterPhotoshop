8bcfcb9eac403e765365d49e68d0d5cb6c393a2c
Bill Williams <eid20021@gmail.com>
2022-10-17 17:25:17 +0300

    fix: YCbCr.601 fix

--- Source/Core/Image/ColorModel/YCbCr601Converter.cpp
+++ Source/Core/Image/ColorModel/YCbCr601Converter.cpp
@@ -1,13 +1,17 @@
 #include "YCbCr601Converter.hpp"
 
 void YCbCr601Converter::ConvertPixelToRGB(Pixel &pixel) {
-    float Y = pixel.channels[0];
-    float Cb = pixel.channels[1];
-    float Cr = pixel.channels[2];
+    float Y = pixel.channels[0] * 255.0f;
+    float Cb = pixel.channels[1] * 255.0f;
+    float Cr = pixel.channels[2] * 255.0f;
 
-    pixel.channels[0] = (Y - 16.0f) / 219.0f + 1.402f * (Cr - 128.0f) / 224;
-    pixel.channels[1] = (Y - 16) / 219 - 1.772f * 0.114f / 0.587f * (Cb - 128.0f) / 224 - 1.402f * 0.299f / 0.587f * (Cr - 128.0f) / 224;
-    pixel.channels[2] = (Y - 16) / 219 + 1.772f * (Cb - 128.0f) / 224;
+    float R = (Y - 16.0f) / 219.0f + 1.402f * (Cr - 128.0f) / 224;
+    float G = (Y - 16) / 219 - 1.772f * 0.114f / 0.587f * (Cb - 128.0f) / 224 - 1.402f * 0.299f / 0.587f * (Cr - 128.0f) / 224;
+    float B = (Y - 16) / 219 + 1.772f * (Cb - 128.0f) / 224;
+
+    pixel.channels[0] = R;
+    pixel.channels[1] = G;
+    pixel.channels[2] = B;
 
 }
 
@@ -17,10 +21,10 @@ void YCbCr601Converter::ConvertPixelFromRGB(Pixel &pixel) {
     float B = pixel.channels[2];
 
     float Y = 16.0f + 65.481f * R + 128.533f * G + 24.966f * B;
-    float Cb = 128.0f - 37.797f * R + 74.203f * G + 112.0f * B;
-    float Cr = 128.0f + 112.0f * R + 93.786f * G + 18.214f * B;
+    float Cb = 128.0f - 37.797f * R - 74.203f * G + 112.0f * B;
+    float Cr = 128.0f + 112.0f * R - 93.786f * G - 18.214f * B;
 
-    pixel.channels[0] = Y;
-    pixel.channels[1] = Cb;
-    pixel.channels[2] = Cr;
+    pixel.channels[0] = Y / 255.0f;
+    pixel.channels[1] = Cb / 255.0f;
+    pixel.channels[2] = Cr / 255.0f;
 }
