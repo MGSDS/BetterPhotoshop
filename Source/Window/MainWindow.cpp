255d2cea2d7be27fa9077237d124793b180c1129
Bill Williams <eid20021@gmail.com>
2022-11-17 11:58:13 +0300

    hotfix: use Utils::NormByte

--- Source/Window/MainWindow.cpp
+++ Source/Window/MainWindow.cpp
@@ -1,4 +1,5 @@
 #include "MainWindow.hpp"
+#include "Core/Utils/Utils.hpp"
 #include "LineDialog.hpp"
 
 #include <Core/Image/ColorModel/ColorModelConverter.hpp>
@@ -537,7 +538,10 @@ void MainWindow::OnImageSelectButtonClick(const QPointF& pos)
         int lineWidth = res[0];
         m_DrawingMode = false;
         if (hasPressedOk) {
-            Pixel color = Pixel(res[1] / 255.0f, res[2] / 255.0f, res[3] / 255.0f, res[4] / 255.0f);
+            Pixel color = Pixel(Utils::NormByte(res[1], 255.0f),
+                                Utils::NormByte(res[2], 255.0f),
+                                Utils::NormByte(res[3], 255.0f),
+                                Utils::NormByte(res[4], 255.0f));
             auto line = Painter::DrawLine(m_Image->GetWidth(), m_Image->GetHeight(),
                                           m_SelectedPoints[0].x(), m_SelectedPoints[0].y(),
                                           m_SelectedPoints[1].x(), m_SelectedPoints[1].y(),
