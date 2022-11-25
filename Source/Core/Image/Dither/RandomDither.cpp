c7d1aa6df4d2619c1901fca0edf529fbcb4f2ecd
Bill Williams <eid20021@gmail.com>
2022-11-25 17:51:19 +0300

    fix: random dither

--- Source/Core/Image/Dither/RandomDither.cpp
+++ Source/Core/Image/Dither/RandomDither.cpp
@@ -13,6 +13,9 @@ static float GetRandomFloat(float maxValue)
 
 std::unique_ptr<Image> RandomDither::Apply(const Image& image, uint8_t bitsPerChannel)
 {
+    //Random dithering
+
+
     if (bitsPerChannel == 0 || bitsPerChannel > 8) {
         throw new std::out_of_range("Specified bitsPerChannel value is out of range.");
     }
@@ -23,7 +26,7 @@ std::unique_ptr<Image> RandomDither::Apply(const Image& image, uint8_t bitsPerCh
     std::unique_ptr<Image> newImage = std::make_unique<Image>(image);
     for (size_t i = 0; i < newImage->GetPixelsCount(); i++) {
         Pixel& pixel = newImage->PixelAt(i);
-        float diff = GetRandomFloat(quantSize) - quantSize;
+        float diff = GetRandomFloat(quantSize) - quantSize / 2.0f;
         for (size_t j = 0; j < 3; j++) {
             pixel.channels[j] = std::clamp(quantSize * std::round((pixel.channels[j] + diff) / quantSize), 0.0f, 1.0f);
         }
