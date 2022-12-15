dcf4d4025a0d6c283baaf9a59f8cb49610250e5c
Clang Robot <robot@example.com>
2022-12-15 18:51:29 +0000

    style: commit clang-format changes

--- Source/Core/Image/Filter/SobelFilter.hpp
+++ Source/Core/Image/Filter/SobelFilter.hpp
@@ -8,6 +8,7 @@ public:
     std::unique_ptr<Image> Apply(const Image& image) override;
     SobelFilter() = default;
     virtual ~SobelFilter() override = default;
+
 private:
     static Pixel GetPixel(const Image& image, int x, int y);
 };
--- Source/Window/MainWindow.cpp
+++ Source/Window/MainWindow.cpp
@@ -627,7 +627,6 @@ void MainWindow::OnDitheringActionSelected(DitherAlgo ditheringType)
 
 void MainWindow::OnApplyFilterAction(FilterAlgo filter)
 {
-
     const std::string& filterName = ENUM_TO_STRING_FILTER_MAPPING.at(filter);
     auto filterParam = ENUM_TO_STRING_FILTER_PARAMETER_MAPPING.at(filter);
     float param = 0.0f;
